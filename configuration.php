11f9b9f05c17e8bf1abd2150cd0f1272b9ff92a5
Binny V A <binnyva@gmail.com>
2015-12-05 01:41:38 +0530

    Done with this. Moving to Angular for this project

--- configuration.php
+++ configuration.php
@@ -1,10 +1,10 @@
 <?php
-//Configuration file for ThagLife
+//Configuration file for TagLife
 $config = array(
-	'site_title'	=>	'ThagLife',
+	'site_title'	=>	'TagLife',
 	'db_host'		=>	'localhost',
 	'db_user'		=>	'root',
 	'db_password'	=>	'',
 	'db_database'	=>	'Project_MyLife',
-	'single_user'	=>	1,
+	// 'single_user'	=>	1,
 );
